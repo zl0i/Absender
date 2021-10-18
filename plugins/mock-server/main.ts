e631c73088fa4c5ce461cfc96dfee4fc34ad3523
zl0i <dmitri.popov.96@mail.ru>
2021-10-18 22:32:44 +0900

    feat(MockPlugin): settings file from argv

--- plugins/mock-server/main.ts
+++ plugins/mock-server/main.ts
@@ -1,12 +1,26 @@
 import express from 'express'
 import fs from 'fs'
 import ResponseBuilder from './responseBuilder'
+import yargs, { strict } from 'yargs'
+import { hideBin } from 'yargs/helpers'
 
 
-var options = JSON.parse(fs.readFileSync('./settings.json').toString())
 
-fs.watchFile('./settings.json', { interval: 1000 }, (_curr, _prev) => {
-    options = JSON.parse(fs.readFileSync('./settings.json').toString())
+const argv = yargs(hideBin(process.argv))
+    .option('file', {
+        alias: 'f',
+        type: 'string',
+        description: 'Path to settings file'
+    })
+    .parse() as any
+
+
+const settings_file: string = argv.f || './settings.json'
+
+var options = JSON.parse(fs.readFileSync(settings_file).toString())
+
+fs.watchFile(settings_file, { interval: 1000 }, (_curr, _prev) => {
+    options = JSON.parse(fs.readFileSync(settings_file).toString())
 })
 
 const app = express()
